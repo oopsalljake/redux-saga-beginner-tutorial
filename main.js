9f84b6350634f21e40579543d27eaf6c0745ec19
Jake Vinson <87077543+oopsalljake@users.noreply.github.com>
2021-08-05 15:56:40 -0400

    add async increment action

--- main.js
+++ main.js
@@ -3,11 +3,18 @@ import "babel-polyfill"
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
 
 import Counter from './Counter'
 import reducer from './reducers'
+import rootSaga from './sagas'
 
-const store = createStore(reducer)
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(
+  reducer,
+  applyMiddleware(sagaMiddleware)
+)
+sagaMiddleware.run(rootSaga)
 
 const action = type => store.dispatch({type})
 
@@ -15,6 +22,7 @@ function render() {
   ReactDOM.render(
     <Counter
       value={store.getState()}
+      onIncrementAsync={() => action('INCREMENT_ASYNC')}
       onIncrement={() => action('INCREMENT')}
       onDecrement={() => action('DECREMENT')} />,
     document.getElementById('root')
